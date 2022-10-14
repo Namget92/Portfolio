8c2fc31ee3fb88842517c635cfa22e48eacbfb8f
Tim Gottling Tegman <92430882+Namget92@users.noreply.github.com>
2022-10-14 15:03:58 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,6 @@ function App() {
         Tim <br /> Gottling <br /> Tegman
       </h1>
       <div className="card">
-        <p>
-          My name is Tim Gottling Tegman and I am currently studying to become a
-          frontend developer at Changemaker Educations in stockholm and I will
-          finish my education next summer. Right now I'm looking for an LIA
-          place (internship), so feel free to contact me via Linkedin if you're
-          interested.
-        </p>
         <div className="icon-container">
           <button
             onClick={() => {
